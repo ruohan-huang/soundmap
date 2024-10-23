78af0c3eaff15f11ba92b46eb8865c85570e57cf
0xsnowyc <0xsnowyc@proton.me>
2024-10-23 16:21:05 -0700

    search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,28 @@
-import React, { useState, useEffect, useRef } from "react";
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import { StyleSheet, View, Text, Button, Alert, Image } from "react-native";
+import React, { useState, useEffect, useRef } from 'react';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Alert,
+  Platform,
+  TextInput,
+} from 'react-native';
 import MapView, {
   Marker,
-  MapPressEvent,
   Region,
   Heatmap,
   LatLng,
-} from "react-native-maps";
-import * as Location from "expo-location";
-import { Audio } from "expo-av";
+} from 'react-native-maps';
+import * as Location from 'expo-location';
+import { Audio } from 'expo-av';
+import { TouchableOpacity, GestureHandlerRootView } from 'react-native-gesture-handler';
 import {
-  TouchableOpacity,
-  GestureHandlerRootView,
-} from "react-native-gesture-handler";
-import {
-  AndroidAudioEncoder,
-  AndroidOutputFormat,
-  IOSAudioQuality,
-  IOSOutputFormat,
   RecordingOptionsPresets,
-} from "expo-av/build/Audio";
+} from 'expo-av/build/Audio';
 
 // Heatmap
-import { predictSoundLevel, fetchOSMData } from "./SoundHeatMap";
+import { predictSoundLevel, fetchOSMData } from './SoundHeatMap';
 
 type WeightedLatLng = LatLng & {
   weight?: number;
@@ -37,15 +36,15 @@ type AudioMarker = {
   audioUri: string;
   dBFS: number;
 };
+
 let record = new Audio.Recording();
+
 export default function HomeScreen() {
   const [region, setRegion] = useState<Region | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [markers, setMarkers] = useState<AudioMarker[]>([]);
-  const [coordinate, setCoordinate] = useState<{
-    latitude: number;
-    longitude: number;
-  }>({ latitude: -1, longitude: -1 });
+  const [coordinate, setCoordinate] = useState<{ latitude: number; longitude: number }>({ latitude: -1, longitude: -1 });
+  const [searchText, setSearchText] = useState<string>('');
 
   // Heatmap variables
   const [heatMapData, setHeatMapData] = useState<WeightedLatLng[]>([]);
@@ -55,8 +54,8 @@ export default function HomeScreen() {
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
         return;
       }
       let location = await Location.getCurrentPositionAsync({});
@@ -73,14 +72,10 @@ export default function HomeScreen() {
   // GET HEATMAP -----------------------------------------------------------------------------------------------------
 
   async function renderHeatMap() {
-    console.log("rendering");
+    console.log('rendering');
     const dividedMap = await getDividedMap();
     console.log(`dividedMap: ${JSON.stringify(dividedMap)}`);
-    const tempData = await fetchOSMData(
-      dividedMap.latArray,
-      dividedMap.lonArray,
-      1000,
-    );
+    const tempData = await fetchOSMData(dividedMap.latArray, dividedMap.lonArray, 1000);
     if (tempData) {
       console.log(tempData);
       setHeatMapData(tempData);
@@ -88,7 +83,6 @@ export default function HomeScreen() {
   }
 
   async function getDividedMap() {
-    // console.log(myMap.current);
     var bounds = await myMap.current?.getMapBoundaries();
     console.log(bounds);
     var topRight = bounds?.northEast; // LatLng of top right corner of map
@@ -99,53 +93,13 @@ export default function HomeScreen() {
     var blLat: number = bottomLeft?.latitude || 0;
     var blLon: number = bottomLeft?.longitude || 0;
 
-    console.log(
-      "bounds: [",
-      trLat,
-      ", ",
-      trLon,
-      "], [",
-      blLat,
-      ", ",
-      blLon,
-      "]",
-    );
+    console.log('bounds: [', trLat, ', ', trLon, '], [', blLat, ', ', blLon, ']');
 
-    // return [getDividedLatitudes(trLat, blLat, 0.5), getDividedLongitudes(trLon, blLon, Math.abs(trLat - blLat) / 2, 0.5)];
     return {
       latArray: getDividedLatitudes(trLat, blLat, 0.5),
-      lonArray: getDividedLongitudes(
-        trLon,
-        blLon,
-        Math.abs(trLat - blLat) / 2,
-        0.5,
-      ),
+      lonArray: getDividedLongitudes(trLon, blLon, Math.abs(trLat - blLat) / 2, 0.5),
     };
   }
-
-  // function distanceBetweenLatitudes(lat1: number, lat2: number) {
-  //   const earthRadius = 6371; // Earth's radius in kilometers
-  //   const deltaLat = Math.abs(lat2 - lat1); // Difference in latitude in degrees
-
-  //   // Convert deltaLat to radians
-  //   const deltaLatRad = deltaLat * (Math.PI / 180);
-
-  //   // Calculate distance
-  //   const distance = earthRadius * deltaLatRad;
-  //   return distance;
-  // }
-
-  // function distanceBetweenLongitudes(lat1: number, lon1: number, lat2: number, lon2: number) {
-  //   const earthRadius = 6371; // Earth's radius in kilometers
-  //   const deltaLon = Math.abs(lon2 - lon1); // Difference in latitude in degrees
-
-  //   // Convert deltaLat to radians
-  //   const deltaLonRad = deltaLon * (Math.PI / 180);
-
-  //   // Calculate distance
-  //   const distance = earthRadius * Math.cos(distanceBetweenLatitudes(lat1, lat2) * (Math.PI / 180)) * deltaLonRad;
-  //   return distance;
-  // }
 
   function getDividedLatitudes(lat1: number, lat2: number, stepSize: number) {
     const deltaLat = Math.abs(lat2 - lat1); // difference in latitudes
@@ -160,24 +114,11 @@ export default function HomeScreen() {
     return latitudes;
   }
 
-  function getDividedLongitudes(
-    lon1: number,
-    lon2: number,
-    lat: number,
-    stepSize: number,
-  ) {
-    const earthRadius = 6371; // Earth's radius in kilometers
-
-    // Convert latitude to radians
-    const latRad = lat * (Math.PI / 180);
-
-    // Calculate the distance between longitudes using the formula:
-    // distance = radius * cos(latitude) * difference in longitude
+  function getDividedLongitudes(lon1: number, lon2: number, lat: number, stepSize: number) {
     const deltaLon = Math.abs(lon2 - lon1); // difference in longitude in degrees
     const longitudes = [];
 
     for (let i = 0; i <= deltaLon / stepSize; i++) {
-      // console.log(i);
       // Calculate the longitude at each step
       const newLon = lon1 + i * stepSize;
       longitudes.push(newLon);
@@ -186,37 +127,24 @@ export default function HomeScreen() {
     return longitudes;
   }
 
-  // const fetchAndPredictSoundLevels = async (latitudes: number[], longitudes: number[], radius: number) => {
-  //   const data = await fetchOSMData(latitudes, longitudes, radius);
-  //   return data;
-  // };
-
-  // useEffect(() => {
-  //   const centerPoint: [number, number] = [47.608013, -122.335167]; // Seattle coordinates
-  //   const radius = 1000; // 1km radius
-  //   // fetchAndPredictSoundLevels(centerPoint, radius);
-  // }, []);
-
   const onRegionChangeComplete = (newRegion: Region) => {
-    // setRegion(newRegion);
     renderHeatMap();
   };
 
   const handleButtonPress = async () => {
     if (coordinate.latitude == -1 && coordinate.longitude == -1) {
-      Alert.alert("Fetching Location", "Move around for better accuracy!", [
+      Alert.alert('Fetching Location', 'Move around for better accuracy!', [
         {
-          text: "Ok",
+          text: 'Ok',
         },
       ]);
       return;
     }
     let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== "granted") {
-      setErrorMsg("Permission to access location was denied");
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
       return;
     }
-    // let location = await Location.getCurrentPositionAsync({});
     await startRecording(coordinate);
   };
 
@@ -230,14 +158,11 @@ export default function HomeScreen() {
 
   // RECORDING ---------------------------------------------------------------------------------------------------------------
 
-  const startRecording = async (coordinate: {
-    latitude: number;
-    longitude: number;
-  }) => {
+  const startRecording = async (coordinate: { latitude: number; longitude: number }) => {
     try {
       const permission = await Audio.requestPermissionsAsync();
-      if (permission.status !== "granted") {
-        setErrorMsg("Permission to access audio was denied");
+      if (permission.status !== 'granted') {
+        setErrorMsg('Permission to access audio was denied');
         return;
       }
       await Audio.setAudioModeAsync({
@@ -249,36 +174,30 @@ export default function HomeScreen() {
       }
       let samples = 0;
       let total = 0;
-      Alert.alert("Recording", "Recording audio. Press stop to finish.", [
+      Alert.alert('Recording', 'Recording audio. Press stop to finish.', [
         {
-          text: "Stop",
+          text: 'Stop',
           onPress: async () => await stopRecording(coordinate, total / samples),
         },
         {
-          text: "Cancel",
+          text: 'Cancel',
           onPress: async () => {
             await record.stopAndUnloadAsync();
           },
         },
       ]);
-      const { recording } = await Audio.Recording.createAsync(
-        RecordingOptionsPresets.HIGH_QUALITY,
-      );
+      const { recording } = await Audio.Recording.createAsync(RecordingOptionsPresets.HIGH_QUALITY);
       record = recording;
       recording.setOnRecordingStatusUpdate((status: Audio.RecordingStatus) => {
         samples++;
         total += status.metering == undefined ? -161 : status.metering;
-        //console.log('Average Volume: ' + (total/samples))
       });
     } catch (err) {
-      console.error("Failed to start recording", err);
+      console.error('Failed to start recording', err);
     }
   };
 
-  const stopRecording = async (
-    coordinate: { latitude: number; longitude: number },
-    avgVolume: number,
-  ) => {
+  const stopRecording = async (coordinate: { latitude: number; longitude: number }, avgVolume: number) => {
     try {
       if (record) {
         console.log(avgVolume);
@@ -287,13 +206,12 @@ export default function HomeScreen() {
         if (uri) {
           setMarkers((prevMarkers) => [
             ...prevMarkers,
-            { coordinate, audioUri: uri, dBFS: avgVolume }, //find dB here
+            { coordinate, audioUri: uri, dBFS: avgVolume }, // find dB here
           ]);
         }
-        //record = undefined;
       }
     } catch (err) {
-      console.error("Failed to stop recording", err);
+      console.error('Failed to stop recording', err);
     }
   };
 
@@ -303,21 +221,57 @@ export default function HomeScreen() {
       const { sound } = await Audio.Sound.createAsync({ uri: audioUri });
       await sound.playAsync();
       Alert.alert(
-        "Playing",
-        "Playing audio (" +
-          Math.round(dBFS * 10.0) / 10.0 +
-          " dBFS). Press stop to finish.",
+        'Playing',
+        'Playing audio (' + Math.round(dBFS * 10.0) / 10.0 + ' dBFS). Press stop to finish.',
         [
           {
-            text: "Stop",
+            text: 'Stop',
             onPress: async () => sound.stopAsync(),
           },
-        ],
+        ]
       );
     } catch (err) {
-      console.error("Failed to play audio", err);
+      console.error('Failed to play audio', err);
     }
     pressed = false;
+  };
+
+  // SEARCH FUNCTIONALITY ------------------------------------------------------------------------------------------------------
+
+  const handleSearch = async () => {
+    if (searchText.trim() === '') {
+      Alert.alert('Empty Search', 'Please enter a location to search.');
+      return;
+    }
+    try {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
+          searchText
+        )}&format=json&limit=1`,
+        {
+          headers: {
+            'User-Agent': 'SoundMap/1.0 (your-email@example.com)', // Replace with your app name and email
+            'Accept-Language': 'en',
+          },
+        }
+      );
+      const data = await response.json();
+      if (data && data.length > 0) {
+        const { lat, lon } = data[0];
+        const newRegion = {
+          latitude: parseFloat(lat),
+          longitude: parseFloat(lon),
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        };
+        setRegion(newRegion);
+        myMap.current?.animateToRegion(newRegion);
+      } else {
+        Alert.alert('Location not found', 'Please try another location.');
+      }
+    } catch (error) {
+      console.error('Error fetching location', error);
+    }
   };
 
   if (!region) {
@@ -330,10 +284,17 @@ export default function HomeScreen() {
   }
 
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search for a location"
+          value={searchText}
+          onChangeText={setSearchText}
+          onSubmitEditing={handleSearch}
+          returnKeyType="search"
+        />
         <MapView
-          // provider={'google'} // changed - error now
           style={styles.map}
           region={region}
           showsUserLocation={true}
@@ -341,8 +302,6 @@ export default function HomeScreen() {
           onRegionChangeComplete={onRegionChangeComplete}
           onUserLocationChange={handleUserLocationUpdate}
           ref={myMap}
-          // onDoublePress={renderHeatMap}
-          //onPress={handleMapPress}
         >
           {markers.map((marker, index) => (
             <Marker
@@ -354,10 +313,10 @@ export default function HomeScreen() {
                 style={{
                   backgroundColor:
                     marker.dBFS >= -28
-                      ? "#ff0000"
+                      ? '#ff0000'
                       : marker.dBFS >= -35
-                        ? "#ff8f00"
-                        : "#007bff",
+                      ? '#ff8f00'
+                      : '#007bff',
                   borderRadius: 100,
                   borderWidth: 1,
                   width: 33,
@@ -367,12 +326,12 @@ export default function HomeScreen() {
                 <TabBarIcon
                   name={
                     marker.dBFS >= -28
-                      ? "volume-high"
+                      ? 'volume-high'
                       : marker.dBFS >= -35
-                        ? "volume-medium"
-                        : "volume-low"
+                      ? 'volume-medium'
+                      : 'volume-low'
                   }
-                  color={"white"}
+                  color={'white'}
                 />
               </View>
             </Marker>
@@ -384,35 +343,39 @@ export default function HomeScreen() {
               heatMapData.length !== 0
                 ? heatMapData
                 : [
-                    { latitude: 47.608013, longitude: -122.335167, weight: 35 },
+                    {
+                      latitude: 47.608013,
+                      longitude: -122.335167,
+                      weight: 35,
+                    },
                     { latitude: 47, longitude: -122.335167, weight: 20 },
                   ]
             }
             radius={50}
             opacity={0.7}
             gradient={{
-              colors: ["green", "yellow", "orange", "red"],
+              colors: ['green', 'yellow', 'orange', 'red'],
               startPoints: [0.01, 0.25, 0.5, 1],
               colorMapSize: 200,
             }}
           />
         </MapView>
-        <View style={{ top: 320, right: -130 }}>
+        <View style={{ position: 'absolute', bottom: 20, right: 20 }}>
           <TouchableOpacity
             style={{
-              backgroundColor: "#007bff",
+              backgroundColor: '#007bff',
               padding: 10,
               borderRadius: 100,
               borderWidth: 1,
               width: 80,
               height: 80,
-              borderColor: "black",
-              alignItems: "center",
-              justifyContent: "center",
+              borderColor: 'black',
+              alignItems: 'center',
+              justifyContent: 'center',
             }}
             onPress={handleButtonPress}
           >
-            <TabBarIcon name={"mic"} color={"white"} style={{ fontSize: 50 }} />
+            <TabBarIcon name={'mic'} color={'white'} style={{ fontSize: 50 }} />
           </TouchableOpacity>
         </View>
       </View>
@@ -423,15 +386,23 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
   markerIcon: {
     width: 40,
+    height: 40,
+  },
+  searchBar: {
+    position: 'absolute',
+    top: Platform.OS === 'ios' ? 60 : 40,
+    width: '90%',
+    alignSelf: 'center',
+    zIndex: 1,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    paddingHorizontal: 10,
     height: 40,
   },
 });
